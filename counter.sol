081b156e22abf9a17490a3a0b0f2e66e02953d9d
hakanharbeli <zh162014>
2022-12-18 21:05:24 +0300

    Odev1

--- counter.sol
+++ counter.sol
@@ -1,2 +1,29 @@
 pragma solidity ^0.8.7;
 //SPDX-License-Identifier: Unlicensed
+
+
+
+contract MyCounter{
+
+    uint public count;
+
+    //get current count
+    function get() public view returns (uint) {
+
+        return count;
+
+    }
+
+    function inc() public {
+        count +=1;
+
+    }
+
+
+    function dec() public{
+        count -= 1;
+    }
+
+
+
+}
